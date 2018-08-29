e4f0e4e5261bcd4595623496da05cbd59ba237e0
shjoo <jshyoung90@icloud.com>
2018-08-29 13:53:48 +0900

    css 넣기 전

--- src/App.js
+++ src/App.js
@@ -2,32 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "모모",
-    poster: "http://tenasia.hankyung.com/webwp_kr/wp-content/uploads/2017/10/2017103016362315546-540x810.jpg"
-  },
-  {
-    title: "쯔위",
-    poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/151020_%ED%8A%B8%EC%99%80%EC%9D%B4%EC%8A%A4_-_%EB%8D%B0%EB%B7%94_%EC%87%BC%EC%BC%80%EC%9D%B4%EC%8A%A4_TWICE_-_Debut_Showcase_%283%29.jpg/200px-151020_%ED%8A%B8%EC%99%80%EC%9D%B4%EC%8A%A4_-_%EB%8D%B0%EB%B7%94_%EC%87%BC%EC%BC%80%EC%9D%B4%EC%8A%A4_TWICE_-_Debut_Showcase_%283%29.jpg"
-  },
-  {
-    title: "정연",
-    poster: "https://i.pinimg.com/originals/4d/6d/c9/4d6dc96fe60686c8a8d6dc635815847d.jpg"
-  },
-  {
-    title: "다현",
-    poster: "http://tenasia.hankyung.com/webwp_kr/wp-content/uploads/2016/05/2016051400070313500-540x810.jpg"
-  }
-]
-
 class App extends Component {
+
+  state = {
+
+  }
+  componentDidMount() {
+    this._getMoives();  
+  }
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie) => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
+    })
+    return movies
+  }
+  _getMoives = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () =>{
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=like_count')
+    .then(potato => potato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
-        { movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        { this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,36 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
 
-class Movie extends Component {
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-
-    render() {
-        console.log(this.props);
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1> { this.props.title } </h1>
-            </div>
-        )
-    }
+function Movie({title, poster}){
+    return(
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1> { title } </h1>
+        </div>
+    )
 }
-
-class MoviePoster extends Component {
-    static PropTypes = {
-        poster: PropTypes.string.isRequired
-    }
-    
-    render() {
-        return(
-            <img src={this.props.poster} />
-        )
-    }
+function MoviePoster({poster}){
+    return(
+        <img src={poster} alt="Moive Poster"/>
+    )
 }
-
+MoviePoster.PropTypes = {
+    poster: PropTypes.string.isRequired
+}
+Movie.PropTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.is
+}
 export default Movie;
